aeaf03f267ccb4ee1b77200e0be0e028a8604bf0
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2023-03-11 15:14:12 -0600

    05-react-buscador-peliculas

---
+++ 05-react-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,23 @@
+const ListOfMovies = ({ movies }) => {
+  return (
+    <ul className='movies'>
+      {movies.map(movie => (
+        <li className='movie' key={movie.id}>
+          <img src={movie.poster} alt={movie.title} />
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const NoMoviesResults = () => {
+  return <p>No se encontraron películas para esta búsqueda</p>;
+};
+
+export const Movies = ({ movies }) => {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />;
+};
